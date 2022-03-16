d544b0a1bd2d974269020e607c1987c27470a0c3
cabalex <31020729+cabalex@users.noreply.github.com>
2022-03-16 04:16:38 -0700

    Hash routing, change description

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import './index.css';
 
