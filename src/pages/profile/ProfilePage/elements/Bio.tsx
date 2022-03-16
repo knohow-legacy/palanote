ccee2451e0a266537d783e2cd19dc238715bb1d9
cabalex <31020729+cabalex@users.noreply.github.com>
2022-03-16 03:50:30 -0700

    Remove username field

--- src/pages/profile/ProfilePage/elements/Bio.tsx
+++ src/pages/profile/ProfilePage/elements/Bio.tsx
@@ -4,7 +4,7 @@ import '../ProfilePage.css';
 function Bio({name, bio, joinDate} : any) {
     return (
         <div className="bio">
-            <h1>{name}<span className="username">@WillWeHaveThese?</span></h1>
+            <h1>{name}</h1>
             <p>{bio}</p>
         </div>
     );
